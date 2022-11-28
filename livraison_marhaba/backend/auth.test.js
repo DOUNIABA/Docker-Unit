3bd024a81e04ec8eaeb2d73822801e8a10e59419
DOUNIA BAHASSANE <dounia0bahassane@gmail.com>
2022-11-28 19:11:19 +0100

    Unit testing-backend

--- livraison_marhaba/backend/auth.test.js
+++ livraison_marhaba/backend/auth.test.js
@@ -11,4 +11,15 @@ describe('Login function', () => {
       })
     expect(res.statusCode).toEqual(200)
   })
+
+
+  it('Erreur Login', async () => {
+    const res = await supertest(app)
+      .post('/api/auth/login')
+      .send({
+        email: "dounhassane@gmail.com",
+        password: "1245",
+      })
+    expect(res.statusCode).toEqual(400)
+  })
 })
